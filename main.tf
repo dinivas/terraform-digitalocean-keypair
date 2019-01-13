9c18944be7c27c8be6236b08e7647692c1eafd73
Rachid <ouattchidi@gmail.com>
2019-01-13 19:18:25 +0100

    minor

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 locals {
-  public_key_filename  = "${var.generate_ssh_key == "true" ? var.public_key_file : ""}"
   private_key_filename = "${format("%s.%s", var.name, "key")}"
 }
 
@@ -10,12 +9,14 @@ resource "openstack_compute_keypair_v2" "this" {
 
 resource "openstack_compute_keypair_v2" "this_provided" {
   count      = "${1 - var.generate_ssh_key}"
+
   name = "${var.name}"
   public_key = "${file("${var.public_key_file}")}"
 }
 
 resource "local_file" "private_key_pem" {
   count      = "${1 - var.generate_ssh_key}"
+
   depends_on = ["openstack_compute_keypair_v2.this_provided"]
   content    = "${openstack_compute_keypair_v2.this_provided.private_key}"
   filename   = "${local.private_key_filename}"
